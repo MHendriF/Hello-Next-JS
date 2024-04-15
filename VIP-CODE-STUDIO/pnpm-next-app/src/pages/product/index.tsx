67ecc71dd2051eba6a34291d7c5c98a852966d72
MHendriF <hendrifebriansyah28@gmail.com>
2024-04-15 11:16:28 +0700

    api routes using firestore

--- VIP-CODE-STUDIO/pnpm-next-app/src/pages/product/index.tsx
+++ VIP-CODE-STUDIO/pnpm-next-app/src/pages/product/index.tsx
@@ -1,17 +1,38 @@
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
+type productType = {
+  id: string;
+  name: number;
+  price: number;
+  size: string;
+};
+
 export default function Product() {
   const [isLogin, isSetLogin] = useState(false);
+  const [products, setProducts] = useState([]);
   const { push } = useRouter();
+  // useEffect(() => {
+  //   if (!isLogin) {
+  //     push("/auth/login");
+  //   }
+  // }, []);
+
   useEffect(() => {
-    if (!isLogin) {
-      push("/auth/login");
-    }
+    fetch("/api/product")
+      .then((res) => res.json())
+      .then((response) => {
+        console.log(response.data);
+        setProducts(response.data);
+      });
   }, []);
+
   return (
     <div>
       <h1>Product</h1>
+      {products.map((product: productType) => (
+        <div key={product.id}>{product.name}</div>
+      ))}
     </div>
   );
 }
