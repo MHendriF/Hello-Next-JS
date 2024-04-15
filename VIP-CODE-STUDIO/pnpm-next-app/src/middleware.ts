9bb0d5a32b567b65394551f2cbfd4053d76671e6
MHendriF <hendrifebriansyah28@gmail.com>
2024-04-15 15:43:52 +0700

    middleware

---
+++ VIP-CODE-STUDIO/pnpm-next-app/src/middleware.ts
@@ -0,0 +1,14 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(req: NextRequest) {
+  const isLogin = false;
+  if (isLogin) {
+    return NextResponse.next();
+  }
+  return NextResponse.redirect(new URL("/auth/login", req.url));
+}
+
+export const config = {
+  matcher: ["/product", "/about"],
+};
--- VIP-CODE-STUDIO/pnpm-next-app/src/pages/product/index.tsx
+++ VIP-CODE-STUDIO/pnpm-next-app/src/pages/product/index.tsx
@@ -5,17 +5,10 @@ import { useEffect, useState } from "react";
 import useSWR from "swr";
 
 export default function ProductPage() {
-  const [isLogin, isSetLogin] = useState(true);
   const [products, setProducts] = useState([]);
   const { push } = useRouter();
 
   const { data, error, isLoading } = useSWR("/api/product", fetcher);
-
-  useEffect(() => {
-    if (!isLogin) {
-      push("/auth/login");
-    }
-  }, []);
 
   return (
     <>
