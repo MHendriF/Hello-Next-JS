4f402be818aa67c81ca6e485dbb2d0a01baa4cd0
MHendriF <hendrifebriansyah28@gmail.com>
2024-04-16 16:35:56 +0700

    change api url

--- VIP-CODE-STUDIO/pnpm-next-app/src/pages/product/[product].tsx
+++ VIP-CODE-STUDIO/pnpm-next-app/src/pages/product/[product].tsx
@@ -30,7 +30,35 @@ export default function DetailProductPage({
   );
 }
 
-// export async function getServerSideProps({
+export async function getServerSideProps({
+  params,
+}: {
+  params: { product: string };
+}) {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/api/product/${params.product}`
+  );
+  const response = await res.json();
+  return {
+    props: {
+      product: response.data,
+    },
+  };
+}
+
+// export async function getStaticPaths() {
+//   const res = await fetch(`http://localhost:3000/api/product`);
+//   const response = await res.json();
+//   const paths = response.data.map((product: ProductType) => ({
+//     params: { product: product.id },
+//   }));
+//   return {
+//     paths,
+//     fallback: false,
+//   };
+// }
+
+// export async function getStaticProps({
 //   params,
 // }: {
 //   params: { product: string };
@@ -45,31 +73,3 @@ export default function DetailProductPage({
 //     },
 //   };
 // }
-
-export async function getStaticPaths() {
-  const res = await fetch(`http://localhost:3000/api/product`);
-  const response = await res.json();
-  const paths = response.data.map((product: ProductType) => ({
-    params: { product: product.id },
-  }));
-  return {
-    paths,
-    fallback: false,
-  };
-}
-
-export async function getStaticProps({
-  params,
-}: {
-  params: { product: string };
-}) {
-  const res = await fetch(
-    `http://localhost:3000/api/product/${params.product}`
-  );
-  const response = await res.json();
-  return {
-    props: {
-      product: response.data,
-    },
-  };
-}
