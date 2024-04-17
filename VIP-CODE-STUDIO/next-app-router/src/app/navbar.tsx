8ceafaa99b55bba53cdaf42b103be2cbeff60f2e
MHendriF <hendrifebriansyah28@gmail.com>
2024-04-17 12:59:51 +0700

    hook usePathname

--- VIP-CODE-STUDIO/next-app-router/src/app/navbar.tsx
+++ VIP-CODE-STUDIO/next-app-router/src/app/navbar.tsx
@@ -1,17 +1,38 @@
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Navbar() {
+  const pathname = usePathname();
+
   return (
     <nav className="flex bg-gray-800 py-2 px-5">
       <ul className="flex ml-5">
-        <Link href={"/"}>
-          <li className="mr-3 text-blue-300 cursor-pointer">Home</li>
+        <Link href="/">
+          <li
+            className={`mr-3 cursor-pointer ${
+              pathname === "/" ? "text-blue-300 " : "text-white"
+            }`}
+          >
+            Home
+          </li>
         </Link>
-        <Link href={"/about"}>
-          <li className="mr-3 text-blue-300 cursor-pointer">About</li>
+        <Link href="/about">
+          <li
+            className={`mr-3 cursor-pointer ${
+              pathname === "/about" ? "text-blue-300 " : "text-white"
+            }`}
+          >
+            About
+          </li>
         </Link>
-        <Link href={"/about/profile"}>
-          <li className="mr-3 text-blue-300 cursor-pointer">Profile</li>
+        <Link href="/about/profile">
+          <li
+            className={`mr-3 cursor-pointer ${
+              pathname === "/about/profile" ? "text-blue-300 " : "text-white"
+            }`}
+          >
+            Profile
+          </li>
         </Link>
       </ul>
     </nav>
