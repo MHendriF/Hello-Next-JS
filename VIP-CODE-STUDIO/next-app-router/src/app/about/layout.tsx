2c2f22fd192e5bcc47d87f4ba2383377a3c0a098
MHendriF <hendrifebriansyah28@gmail.com>
2024-04-17 12:24:35 +0700

    layout vs template

--- VIP-CODE-STUDIO/next-app-router/src/app/about/layout.tsx
+++ VIP-CODE-STUDIO/next-app-router/src/app/about/layout.tsx
@@ -1,11 +1,16 @@
+"use client";
+import { useState } from "react";
+
 export default function AboutLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const [state, setState] = useState(0);
+
   return (
     <>
-      <nav className="fixed left-0 top-10 z-10 h-screen w-60 bg-gray-800">
+      <nav className="fixed right-0 top-10 z-10 h-screen w-60 bg-gray-800">
         <ul className="text-white p-5">
           <li>Home</li>
           <li>About</li>
