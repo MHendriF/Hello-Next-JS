97c8c78ad91a43573fc57cc2b6fb59defe7168b2
MHendriF <hendrifebriansyah28@gmail.com>
2024-04-17 10:47:16 +0700

    setup project

---
+++ VIP-CODE-STUDIO/next-app-router/src/app/about/page.tsx
@@ -0,0 +1,3 @@
+export default function AboutPage() {
+  return <h1 className="text-3xl font-bold underline">About Page</h1>;
+}
---
+++ VIP-CODE-STUDIO/next-app-router/src/app/about/profile/page.tsx
@@ -0,0 +1,3 @@
+export default function ProfilePage() {
+  return <h1 className="text-3xl font-bold underline">Profile Page</h1>;
+}
