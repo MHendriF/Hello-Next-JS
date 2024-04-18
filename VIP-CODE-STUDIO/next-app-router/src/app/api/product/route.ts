8c829da26d4da9c44b01f49f36eeb5b457ad7b66
MHendriF <hendrifebriansyah28@gmail.com>
2024-04-18 09:53:25 +0700

    data fetching

--- VIP-CODE-STUDIO/next-app-router/src/app/api/product/route.ts
+++ VIP-CODE-STUDIO/next-app-router/src/app/api/product/route.ts
@@ -5,16 +5,22 @@ const data = [
     id: 1,
     name: "Baju",
     price: 100000,
+    image:
+      "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/82ebffbc-7ba5-4ebb-85d7-03d47d802bd9/killshot-2-leather-shoes-1DJNnt.png",
   },
   {
     id: 2,
     name: "Sepatu",
     price: 300000,
+    image:
+      "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/82ebffbc-7ba5-4ebb-85d7-03d47d802bd9/killshot-2-leather-shoes-1DJNnt.png",
   },
   {
     id: 3,
     name: "Celana",
     price: 400000,
+    image:
+      "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/82ebffbc-7ba5-4ebb-85d7-03d47d802bd9/killshot-2-leather-shoes-1DJNnt.png",
   },
 ];
 
