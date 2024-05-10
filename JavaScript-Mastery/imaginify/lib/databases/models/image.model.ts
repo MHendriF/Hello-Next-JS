8a9b7d2319f805194f08ed841f9a039585be6406
MHendriF <hendrifebriansyah28@gmail.com>
2024-05-10 10:31:11 +0700

    create model db

--- JavaScript-Mastery/imaginify/lib/databases/models/image.model.ts
+++ JavaScript-Mastery/imaginify/lib/databases/models/image.model.ts
@@ -1,4 +1,25 @@
-import { Schema, model, models } from "mongoose";
+import { Document, Schema, model, models } from "mongoose";
+
+export interface IImage extends Document {
+  title: string;
+  transformationType: string;
+  publicId: string;
+  secureUrl: string;
+  width?: number;
+  height?: number;
+  config?: object;
+  transformationUrl: URL;
+  aspecRation?: string;
+  color?: string;
+  prompt?: string;
+  author: {
+    _id: string;
+    firstName: string;
+    lastName: string;
+  };
+  createdAt?: Date;
+  updatedAt?: Date;
+}
 
 const ImageSchema = new Schema({
   title: { type: String, required: true },
@@ -11,7 +32,7 @@ const ImageSchema = new Schema({
   transformationUrl: { type: URL },
   aspecRation: { type: String },
   color: { type: String },
-  promp: { type: String },
+  prompt: { type: String },
   author: { type: Schema.Types.ObjectId, ref: "User" },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
