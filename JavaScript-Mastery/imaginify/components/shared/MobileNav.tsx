aefe44085b5e99ea86ba3a4b2465ebb0cc30a28c
MHendriF <hendrifebriansyah28@gmail.com>
2024-05-10 10:49:09 +0700

    setup models and actions

--- JavaScript-Mastery/imaginify/components/shared/MobileNav.tsx
+++ JavaScript-Mastery/imaginify/components/shared/MobileNav.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
-import { navLinks } from "@/constant";
+import { navLinks } from "@/constants";
 import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
--- JavaScript-Mastery/imaginify/components/shared/Sidebar.tsx
+++ JavaScript-Mastery/imaginify/components/shared/Sidebar.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { navLinks } from "@/constant";
+import { navLinks } from "@/constants";
 import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
